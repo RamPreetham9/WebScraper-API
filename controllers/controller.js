2f9c5482e9d2c922825898559d1a2b6bb44c2ec8
chefgordon42069 <rampreethamt@gmail.com>
2023-04-03 15:46:36 +0530

    cors added

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,8 +2,8 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const Scrape = async (req, res) => {
-    const {url} = req.body;
-    axios.get(url)
+    const {myurl} = req.body;
+    axios.get(myurl)
         .then(response => {
             const html = response.data;
             const $ = cheerio.load(html);
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const Routes = require('./routes/route.js');
+const cors=require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method);
